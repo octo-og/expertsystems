970da1c8e07882378f54468bab9f43c9a6383aa0
david <lutalidavid44@gmail.com>
2023-03-01 18:55:02 +0300

    updated db ctr

--- src/src/databaseControl.java
+++ src/src/databaseControl.java
@@ -33,22 +33,22 @@ public class databaseControl {
         return resultSet;
     }
 
-    public ArrayList<String> times()
+    public ArrayList<String> name()
     {
 
-        ArrayList<String> time =new ArrayList<String>();
+        ArrayList<String> dbName =new ArrayList<String>();
         try {
 
             Class.forName("com.mysql.jdbc.Driver");
             Connection con= DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/movie","deh","bedrock");
+                    "jdbc:mysql://localhost:3306/expert","devi","pass");
             Statement stmt=con.createStatement();
             ResultSet rs=stmt.executeQuery(" select time from movie;");
             while(rs.next()){
 
                 productControl name =  new productControl();
                 name.setName(rs.getString("time"));
-                time.add(name.getName());
+                dbName.add(name.getName());
             }
 
             con.close();
@@ -57,6 +57,110 @@ public class databaseControl {
             System.out.println(throwable);
         }
 
-        return time;
+        return dbName;
+    }
+    public ArrayList<String> colour()
+    {
+
+        ArrayList<String> dbColour =new ArrayList<String>();
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con= DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/expert","devi","pass");
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery(" select time from movie;");
+            while(rs.next()){
+
+                productControl name =  new productControl();
+                name.setColor(rs.getString("time"));
+                dbColour.add(name.getColor());
+            }
+
+            con.close();
+        }
+        catch (SQLException | ClassNotFoundException throwable) {
+            System.out.println(throwable);
+        }
+
+        return dbColour;
+    }
+    public ArrayList<Float> size()
+    {
+
+        ArrayList<Float> dbSize =new ArrayList<Float>();
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con= DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/expert","devi","pass");
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery(" select time from movie;");
+            while(rs.next()){
+
+                productControl name =  new productControl();
+                name.setSize(rs.getFloat("time"));
+                dbSize.add(name.getSize());
+            }
+
+            con.close();
+        }
+        catch (SQLException | ClassNotFoundException throwable) {
+            System.out.println(throwable);
+        }
+
+        return dbSize;
+    }
+    public ArrayList<Float> weight()
+    {
+
+        ArrayList<Float> dbWeight =new ArrayList<Float>();
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con= DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/expert","devi","pass");
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery(" select time from movie;");
+            while(rs.next()){
+
+                productControl name =  new productControl();
+                name.setWeight(rs.getFloat("time"));
+                dbWeight.add(name.getWeight());
+            }
+
+            con.close();
+        }
+        catch (SQLException | ClassNotFoundException throwable) {
+            System.out.println(throwable);
+        }
+
+        return dbWeight;
+    }
+    public ArrayList<Float> price()
+    {
+
+        ArrayList<Float> dbPrice =new ArrayList<Float>();
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con= DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/expert","devi","pass");
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery(" select time from movie;");
+            while(rs.next()){
+
+                productControl name =  new productControl();
+                name.setPrice(rs.getFloat("time"));
+                dbPrice.add(name.getPrice());
+            }
+
+            con.close();
+        }
+        catch (SQLException | ClassNotFoundException throwable) {
+            System.out.println(throwable);
+        }
+
+        return dbPrice;
     }
 }
